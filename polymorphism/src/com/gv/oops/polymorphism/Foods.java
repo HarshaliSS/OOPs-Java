e24879ff0676d1eed03ba0e0b0af6feecba67429
USER <USER@LAPTOP-HH97ENEF>
2024-07-13 21:06:37 +0530

    commit

---
+++ polymorphism/src/com/gv/oops/polymorphism/Foods.java
@@ -0,0 +1,45 @@
+package com.gv.oops.polymorphism;
+
+public class Foods extends Products {
+
+	private String expiryDate;
+	private boolean veg;
+
+	public Foods() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Foods(String expiryDate, boolean veg) {
+		super();
+		this.expiryDate = expiryDate;
+		this.veg = veg;
+	}
+
+	public String getExpiryDate() {
+		return expiryDate;
+	}
+
+	public void setExpiryDate(String expiryDate) {
+		this.expiryDate = expiryDate;
+	}
+
+	public boolean isVeg() {
+		return veg;
+	}
+
+	public void setVeg(boolean veg) {
+		this.veg = veg;
+	}
+
+	@Override
+	public String getProduct() {
+		return "Brand:" + getBrand() + " price:" + getPrice() + " ExpiryDate:" + expiryDate + " veg:" + veg;
+	}
+	
+	@Override
+	public String toString() {
+		return "Food [expiryDate=" + expiryDate + ", veg=" + veg + "]";
+	}
+
+}
---
+++ polymorphism/src/com/gv/oops/polymorphism/MainProducts.java
@@ -0,0 +1,37 @@
+package com.gv.oops.polymorphism;
+
+
+public class MainProducts {
+
+	public static void main(String[] args) {
+		Products product = new Products();
+		Electronics charger = new Electronics();
+		Foods milk = new Foods();
+		
+		charger.setBrand("1Plus");
+		charger.setPrice(1000.40F);
+		charger.setBattery(false);
+		charger.setWireless(false);
+		
+		
+		milk.setBrand("Gokul");
+		milk.setPrice(30.15F);
+		milk.setExpiryDate("07-06-2024");
+		milk.setVeg(true);
+		
+		
+		System.out.println(product.getProduct());
+		
+		
+		// Polymorphism 
+		//upcasting
+		product=charger;
+		System.out.println(product.getProduct());
+		System.out.println(charger.getProduct1());
+		
+		//upcasting 
+		product=milk;
+		System.out.println(product.getProduct());
+		
+	}
+}

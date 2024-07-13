798f378dc32a8134b9030c767f166cd1d99a2aee
USER <USER@LAPTOP-HH97ENEF>
2024-07-13 20:50:28 +0530

    commit

---
+++ encapsulation/src/com/gv/oops/encapsulation/MainAddition.java
@@ -0,0 +1,11 @@
+package com.gv.oops.encapsulation;
+ 
+public class MainAddition {
+
+	public static void main(String[] args) {
+		Addition addition = new Addition();
+		addition.setNumber1(10);
+		addition.setNumber2(20);
+		addition.addTwoNumber();
+	}
+}
